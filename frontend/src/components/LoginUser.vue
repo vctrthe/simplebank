417f3aed2269a0d2bf067cb9853d54037fb06277
vctrthe <victorthe123@gmail.com>
2026-05-11 21:05:48 +0700

    feat: added login button, axios router, and login function

--- frontend/src/components/LoginUser.vue
+++ frontend/src/components/LoginUser.vue
@@ -2,6 +2,20 @@
 import InputGroup from 'primevue/inputgroup'
 import InputGroupAddon from 'primevue/inputgroupaddon'
 import { InputText } from 'primevue'
+import FloatLabel from 'primevue/floatlabel'
+import { computed, ref } from 'vue'
+import Button from 'primevue/button'
+import axios from 'axios'
+
+const username = ref<string>('')
+const password = ref<string>('')
+const isLoginDisabled = computed(() => !username.value || !password.value)
+const handleLogin = async () => {
+  const response = await axios.post('http://localhost:8080/v1/login_user', {
+    username: username.value,
+    password: password.value
+  })
+}
 </script>
 
 <template>
@@ -10,15 +24,23 @@ import { InputText } from 'primevue'
       <InputGroupAddon>
         <i class="pi pi-user"></i>
       </InputGroupAddon>
-      <InputText v-model="text1" placeholder="Username" />
+      <FloatLabel>
+        <InputText id="Username" v-model="username" />
+        <label for="Username">Username</label>
+      </FloatLabel>
     </InputGroup>
 
     <InputGroup>
       <InputGroupAddon>
         <i class="pi pi-lock"></i>
       </InputGroupAddon>
-      <InputText v-model="text1" type="password" placeholder="Password" />
+      <FloatLabel>
+        <InputText type="password" id="Password" v-model="password" />
+        <label for="Password">Password</label>
+      </FloatLabel>
     </InputGroup>
+
+    <Button label="Login" :disabled="isLoginDisabled" @click="handleLogin" />
   </div>
 </template>
 
